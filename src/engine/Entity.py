fbafb973a2bf6f442a89191de9d1c450066603d4
benjimania74 <Benjamin.Bogdan@etu.univ-smb.fr>
2025-09-28 15:45:21 +0200

    Système de collision prévisionnel

--- src/engine/Entity.py
+++ src/engine/Entity.py
@@ -1,63 +1,55 @@
 from .Object import MoveableObject, Collideable
 
 class Entity(MoveableObject, Collideable):
-    def willCollideWhen(self, c: Collideable) -> tuple[tuple[float, float], tuple[float, float]]:
+    def willCollideWhen(self, c: Collideable, xMove: int, yMove: int) -> tuple[float, float]:
         """
-        Renvoie les coordonnées auquelles l'entité et le Collideable entrereont en collision et sortiront de leur collision durant le mouvement de l'entité.
-        Renvoie ( (-1, -1), (-1, -1) ) si il n'y a pas de collision
+        Renvoie les coefficients (du vecteur de déplacement) d'entrée et de sortie de la collision frontale entre l'entité et 'c'.
+        Si il n'y a pas de collision, (-1,-1) est retourné
         """
-        t_xEnter: float
-        t_xOut: float
-        t_yEnter: float
-        t_yOut: float
+        t_xEnter: float = -1
+        t_xOut: float = -1
+        t_yEnter: float = -1
+        t_yOut: float = -1
 
-        if self.xMove == 0:
+        if xMove == 0:
             if c.x <= self.x + self.width and self.x <= c.x + c.width:
                 t_xEnter = 0
                 t_xOut = 1
-            else:
-                t_xEnter = -1
-                t_xOut = -1
+
+                if c.x == self.x + self.width or self.x == c.x + c.width: # on bouge sur l'axe des ordonnées et on est collé à gauche ou à droite
+                    t_yEnter = 1
+                    t_yOut = 1
         else:
-            A: float = ( c.x - self.x - self.width ) / self.xMove
-            B: float = ( c.x + c.width - self.x ) / self.xMove
+            A: float = ( c.x - self.x - self.width ) / xMove
+            B: float = ( c.x + c.width - self.x ) / xMove
             t_xEnter = min(A, B)
             t_xOut = max(A,B)
         
-        if self.yMove == 0:
-            if c.y <= self.y + self.height and self.y <= c.y + c.height:
-                t_yEnter = 0
-                t_yOut = 1
+        if t_yEnter == -1: # si on a pas déjà estimé le déplacement sur l'axe vertical
+            if yMove == 0:
+                if c.y <= self.y + self.height and self.y <= c.y + c.height:
+                    t_yEnter = 0
+                    t_yOut = 1
+                if c.y == self.y + self.height or self.y == c.y + c.height: # on bouge sur l'axe des abscisses et on est collé en bas ou en haut
+                    t_xEnter = 1
+                    t_xOut = 1
             else:
-                t_yEnter = -1
-                t_yOut = -1
-        else:
-            A: float = ( c.y - self.y - self.height ) / self.yMove
-            B: float = ( c.y + c.height - self.y ) / self.yMove
-            t_yEnter = min(A, B)
-            t_yOut = max(A,B)
+                A: float = ( c.y - self.y - self.height ) / yMove
+                B: float = ( c.y + c.height - self.y ) / yMove
+                t_yEnter = min(A, B)
+                t_yOut = max(A,B)
 
-        res = ( (-1, -1), (-1, -1) )
+        res = (-1, -1)
 
         t_enter: float = max(t_xEnter, t_yEnter)
         t_out: float = min(t_xOut, t_yOut)
 
         if t_enter < 0 and 0 <= t_out:
             t_enter = 0
-        
         if 1 < t_out and t_enter < 1:
             t_out = 1
-
+        
         if 0 <= t_enter and t_enter <= t_out and t_out <= 1:
-            res = (
-                (
-                    self.x + t_enter * self.xMove,
-                    self.y + t_enter * self.yMove
-                ),
-                (
-                    self.x + t_out * self.xMove,
-                    self.y + t_out * self.yMove
-                )
-            )
+            res = (t_enter, t_out)
 
         return res
