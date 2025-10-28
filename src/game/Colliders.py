cfb652970bd1c59efdfe843173d7acc07df09e1e
benjimania74 <Benjamin.Bogdan@etu.univ-smb.fr>
2025-10-28 18:00:41 +0100

    images sur RectangleCollirders et transparence couleur

--- src/game/Colliders.py
+++ src/game/Colliders.py
@@ -1,44 +1,48 @@
 from engine.Object import Collideable
 from game.Utils import toPygameY
 from game.Player import Player
+from game.Image import Image
 
-from pygame.surface import Surface
-from pygame.color import Color
-from pygame.rect import Rect
-from pygame import transform
+from pygame import Surface, Color, SRCALPHA
 
 class RectangleCollider(Collideable):
-    color: Color|None
+    color: Color|None = None
+    image: Image|None = None
     drawSurface: Surface
 
-    def __init__(self, x: int, y: int, w: int, h: int, priority: int, color: Color|None, surface: Surface) -> None:
+    def __init__(self, x: int, y: int, w: int, h: int, priority: int, texture: Color|str|None, surface: Surface) -> None:
         self.x = x
         self.y = y
         self.width = w
         self.height = h
         self.priority = priority
-        self.color = color
+        if texture == None or isinstance(texture, Color):
+            self.color = texture
+        elif texture != "":
+            self.image = Image(x,y,w,h,texture,surface)
         self.drawSurface = surface
     
     def show(self):
-        if self.color == None or self.color.a == 0:
-            return
-        toDisplay: Surface = Surface((self.width,self.height))
-        toDisplay.fill(self.color)
-        self.drawSurface.blit(
-            transform.scale(toDisplay, (self.width, self.height)),
-            (
-                self.x,
-                toPygameY(self.y, self.height, self.drawSurface.get_height())
+        toDisplay: Surface = Surface((self.width, self.height), SRCALPHA)
+        if self.color != None:
+            if self.color.a == 0: return # rien a afficher vu que c'est totalement transparent
+            toDisplay.fill(self.color)
+            self.drawSurface.blit(
+                toDisplay,
+                (
+                    self.x,
+                    toPygameY(self.y, self.height, self.drawSurface.get_height())
+                )
             )
-        )
+        elif self.image != None:
+            self.image.show()
 
 class PlayerDetectorCollider(RectangleCollider):
     player: Player
     
-    def __init__(self, x: int, y: int, w: int, h: int, priority: int, color: Color, surface: Surface, player: Player) -> None:
+    def __init__(self, x: int, y: int, w: int, h: int, priority: int, texture: Color|str, surface: Surface, player: Player) -> None:
         self.player = player
-        super().__init__(x, y, w, h, priority, color, surface)
+        super().__init__(x, y, w, h, priority, texture, surface)
     
     def collidePlayer(self) -> bool:
         """Retourne si le joueur touche le detecteur"""
